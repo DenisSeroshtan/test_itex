1e38c788c5effdead678e712f7d9fdef7d68a093
DenisSeroshtan <haron_777@mail.ru>
2017-12-08 14:33:02 +0500

    finish

--- gulp/paths/app.js
+++ gulp/paths/app.js
@@ -1,9 +1,9 @@
 'use strict';
 
 module.exports = [
-   "./source/js/preloader.js",
    "./source/js/slider.js",
-   "./source/js/map_ya.js",
-   "./source/js/todo.js"
+  "./source/js/acco.js",
+   "./source/js/todo.js",
+  "./source/js/map_ya.js"
 
 ];
